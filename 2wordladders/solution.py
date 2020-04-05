af0976678d56074d3410a0ded20c60da62ad3850
Henrik Norrman <norrman92@gmail.com>
2020-04-05 17:37:17 +0200

    new file:   2wordladders/solution.py

---
+++ 2wordladders/solution.py
@@ -0,0 +1,20 @@
+import sys
+
+def get_data():
+    print("get_data")
+
+
+def BFS():
+    print("BFS")
+
+
+def print_results():
+    print("results")
+
+def main():                                     
+    get_data()                    
+    BFS() 
+    print_results()                   
+
+
+if __name__== "__main__": main()
