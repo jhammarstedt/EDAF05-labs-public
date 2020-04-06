0f5680d24776dd7bfa158ab10aa2879caba49ebf
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-04-06 15:33:50 +0200

    added graph building function

--- 2wordladders/solution.py
+++ 2wordladders/solution.py
@@ -10,36 +10,63 @@ words = ['there','where','input','putin','hello']
 queeries = [['there','where'],
             ['putin','input'],
             ['hello','putin']]   
-def build_graph():
-    print('Building graph')
-class Node:
-    def __init__(self,data=None):
-        self.data = data
-        self.next = None
-class LinkedList():
+
+def create_graph():
+    class Vertex:
+        def __init__ (self,node):
+            """Getting the id for the node and a adjacent 
+            dict to store it's neighbours"""
+            self.id = node
+            self.adjacent = []
+        
+        def get_connections(self):
+            """display node connections"""
+            connections = [x.id for x in self.adjacent]
+            print(f'{self.id} is connected to {connections}')
+            return self.adjacent
+        def get_id(self):
+            return self.id
+        
+        def add_neighbor(self, neighbor):
+            self.adjacent.append(neighbor)
+            
     
-    def __init__(self):
-        self.head = None
+    class Graph:
+        def __init__(self):
+            self.vert_dict = {}
+            self.num_vertices = 0
     
-    def printing(self):
-        printing = self.head
-        while printing is not None:
-            print(printing.data)
-            printing = printing.next
-    def AtBegining(self,newdata):
-        NewNode = Node(newdata)
     
-        #Update the new nodes next to existing ndoe
-        NewNode.next = self.head
-        self.head = NewNode
-#
-ll = LinkedList()
-ll.head = Node(1)
-n2 = Node(2)
-n3 = Node(3)
-ll.head.next= n2
-n2.next=n3
-#build_graph()
+        def add_vertex(self, node):
+            self.num_vertices = self.num_vertices + 1
+            new_vertex = Vertex(node)
+            self.vert_dict[node] = new_vertex
+            return new_vertex
+    
+        def get_vertex(self, n):
+            if n in self.vert_dict:
+                return self.vert_dict[n]
+            else:
+                return None
+    
+        def add_edge(self, frm, to, cost = 0):
+            if frm not in self.vert_dict:
+                self.add_vertex(frm)
+            if to not in self.vert_dict:
+                self.add_vertex(to)
+    
+            self.vert_dict[frm].add_neighbor(self.vert_dict[to])
+            self.vert_dict[to].add_neighbor(self.vert_dict[frm])
+    
+        def get_vertices(self):
+            return self.vert_dict.keys()
+    
+    #Testing to build some connections
+    g = Graph()
+    g.add_vertex('a')
+    g.add_vertex('b')
+    g.add_edge('a', 'b') 
+    g.vert_dict['a'].get_connections()
 #%%
 
 def BFS():
