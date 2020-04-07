62b94ad0e8661405cc7dc94234ba01bc1dc0b367
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-04-07 14:23:53 +0200

    Update solution.py

--- 2wordladders/solution.py
+++ 2wordladders/solution.py
@@ -68,7 +68,7 @@ def BFS(start, goal, graph):
                 granne.check=True
                 q.append(granne)
                 if granne == goal_node:
-                    print('Found path! Lenght: ',len(q))
+                    print('Found path! Lenght: ',len(q))                    
                     return len(q)
         
    
