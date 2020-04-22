d91f7bcf16161e0c7554ae7359ccf05487e624e9
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-04-22 14:18:02 +0200

    updates

--- 3makingfriends/graph.py
+++ 3makingfriends/graph.py
@@ -4,19 +4,34 @@ Created on Wed Apr 15 21:00:31 2020
 
 @author: johan
 """
+def union(node1,node2):     #check if they are in the same set
+    #print(f'node1.set : {node1.set}')
+    #print(f'node2.set : {node2.set}')
+    if (node1.set == node2.set):    #If both None they are not in the same set
+        return True
+    else: 
+        return False
 
-def MakeUnionFind(S):
-    class node:
-        def __init__(self):
-            self.point = None           #Points towards the set it's in, initialized to it's own
-            
-
-
-class Graph:
+class node:
+    def __init__(self,id):
+        self.point = None       #The edge its connected too       
+        self.set = id           #The set it points towards, first none= to itself
+        self.setsize = 1        #The size of the set
+        self.id = id            #Just for own reference
+    def addEdge(self,node2):
+        self.point = node2      # make it point towards the new node
+        
+    def merge(self,node2):
+        self.set = node2.set   # Merge this node into the other nodes set
+        node2.setsize +=1       # increment the setsize 
+        
+#%%        
+"""also from his psudo"""      
+class sets:
     def __init__(self,nodes):
         self.nodes= nodes                #Vertices
-        self.components = []             #default dict to store graph
-        
+        #self.components = []             #default dict to store graph
+        #self.set = None                 #The set in which the node is connected to
     
     def addEdge(self,u,v,weight):
         """Add an edge to graph"""
--- 3makingfriends/solution.py
+++ 3makingfriends/solution.py
@@ -15,16 +15,43 @@ with open(path) as f:
     a= f.read().split("\n")
     print(a)
     
+#%%
+people = 4                                                      # how many people 
+pairs = 4                                                       # how many connections we will do
+all_data = [[1,2,1],
+        [1,3,7],
+        [2,3,4],
+        [3,4,5]]       # 
 
-people = 3                  # how many people 
-pairs = 2                   # how many connections we will do
-b = [[1,2,3],[2,3,7]]       # 
 
+all_data.sort(key=lambda x: x[2])                               #sorting the data according to weight
+all_nodes= []
+for i in range(0,people):                                       #init all nodes as empty sets O(n)
+    all_nodes.append(node(id=(i+1)))
 
-
-
-
-
+def krus(nodes,data):
+    big_set = []
+    edges = data.copy()
+    while len(edges) !=0:
+        current_edge = edges.pop(0)                             #taking out the first edge
+        start_node = nodes[current_edge[0]-1]                   #for reference we take out the nodes as well
+        end_node = nodes[current_edge[1]-1]
+        #print(f'start_node id:{start_node.id} and set {start_node.set}')
+        #print(f'end_node id:{end_node.id} and set {end_node.set}')
+        if not union(start_node,end_node):                     #if they're not in the same set
+            #print(f'Adding edge : {current_edge}')   
+            if start_node.setsize < end_node.setsize:                   
+                start_node.merge(end_node)
+            else:
+                end_node.merge(start_node)
+            big_set.append(current_edge)                        #adding the edge to our final set
+        #else do nothing since we don't want this node due to circut
+    return big_set
+final_set = krus(all_nodes,all_data)
+total_cost = 0
+for i in final_set:
+    total_cost+=i[2]
+print(f'total cost {total_cost}')  
 #%%
 """This is just taken from his psudo code"""
 def makeunionfind(S):
