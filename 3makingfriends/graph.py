75bfd11e8581bce3025666b1b05f11890827f98b
Henrik Norrman <norrman92@gmail.com>
2020-04-26 03:37:18 +0200

    clean-up

--- 3makingfriends/graph.py
+++ 3makingfriends/graph.py
@@ -1,126 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 15 21:00:31 2020
-
-@author: johan
-"""
-
-
 class Graph:
-    def __init__(self):
-        self.edge_roots = {}
+    def __init__(self, size):
+        self.edge_roots = [0] * (size + 2)                  # empty int[] to store roots
+
 
     def find(self, node):
-        if node not in self.edge_roots:
-            self.edge_roots[node] = node
-        #self.edge_roots[node].setdefault(node)
-        selected_node = self.edge_roots[node]
-        #selected_node = self.edge_roots.get(node, node)
-        
-        #print(node, ":", selected_node)
-        if selected_node == node:
-            return node
+        if self.edge_roots[node] == 0:                      # if node not in edge_roots...
+            self.edge_roots[node] = node                    # ...node is it's own root
 
+        root = self.edge_roots[node]                        
 
-        #self.find(selected_node)
-            
-        self.edge_roots[node] = self.find(selected_node)
+        if root == node:                                    
+            return node                                     # stop resursive search when root == node
+
+        self.edge_roots[node] = self.find(root)             # [RECURSIVE] node.root <- root.root
         return self.edge_roots[node]
 
+    
     def union(self, edge):
-        #print(edge)
+        u = min(edge[0], edge[1])                           # node u of edge (u, v)
+        v = max(edge[0], edge[1])                           # node v of edge (u, v)
 
-        if edge[0] < edge[1]:
-            a = edge[0]
-            b = edge[1]
+        if self.edge_roots[v] != 0:                         # if v is already in graph, and already has a root...
+            self.edge_roots[self.find(v)] = self.find(u)    # ...set that root to the root of u
         else:
-            a = edge[1]
-            b = edge[0]
-
-        if b in self.edge_roots:
-            #print("-------", b ,"---------")
-            self.edge_roots[self.find(b)] = self.find(a)
-            #print("print:", self.find(b))
-            #print("-------finish---------")
-        else:
-            self.edge_roots[b] = self.find(a)
-
-        self.edge_roots[a] = self.find(a)
-        
-
-       #print(self.edge_roots)
-               
-
-def union(node1,node2):             #check if they are in the same set
-    if (node1.set == node2.set):    #If both None they are not in the same set
-        return True
-    else: 
-        return False
-
-class node:
-    def __init__(self,id):
-        self.point = None       #The edge it's connected too -- Not used now, do we need it?      
-        self.set = id           #The set it points towards, first to itself, the set is the id of a node in that set
-        self.setsize = 1        #The size of the set
-        self.id = id            #Just for own reference
- 
-        
-    def merge(self,node2):
-        self.set = node2.set    # Merge this node into the other nodes set
-        node2.setsize +=1       # increment the setsize
+            self.edge_roots[v] = self.find(u)               # ...otherwise v.root <- u.root
 
 
-    def addEdge(self,node2):    #not used, needed?
-        self.point = node2      # make it point towards the new node
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-#%%        
-"""also from his psudo, not used"""      
-class sets:
-    def __init__(self,nodes):
-        self.nodes= nodes                #Vertices
-        #self.components = []             #default dict to store graph
-        #self.set = None                 #The set in which the node is connected to
-    
-    def addEdge(self,u,v,weight):
-        """Add an edge to graph"""
-        self.graph.append([u,v,weight])
-    
-    def find(self,parent,v):
-        """Find the canonical member of the set which v belongs to"""
-        p = v
-        while len(parent[p])!= 0:
-            temp = parent[v]
-            parent[v] = p
-            v = temp
-        return p
-    
-    def union(self,u,v):
-        """Merge the two sets p_i and p_j represented by u and v into a new set p_k represented by u,
-        and destroy p_i and p_j"""
-        u_root = self.find(u)
-        v_root = self.find(v)
-        if len(u_root) < len(v_root): #this is probably wrong, should maybe be rank
-            parent[u_root] = v_root
-            #size(v) = size(u)+size(v)
-            
-        else:
-            parent[v_root]=u_root
-            #size(u)=size(u)+size(v)
-     
--- 3makingfriends/solution.py
+++ 3makingfriends/solution.py
@@ -1,133 +1,45 @@
 import sys
 
 from collections import deque
-from graph import Graph,union,node
+from graph import Graph
 
-
-
-
-#def get_local_data():
-#    """ Returns the formatted data fetched from local. """
-#         
-#    #path = r"C:\Users\johan\Documents\GitHub\EDAF05-labs\3makingfriends\data\sample\1.in"
-#    path= r"C:\Users\johan\Documents\GitHub\EDAF05-labs\3makingfriends\data\sample\1.in"
-#    with open(path) as f:
-#        raw_data= f.read().split("\n")
-#    info = raw_data[0].split(' ')
-#    people = int(info[0])
-#    pairs = int(info[1])
-#    
-#    all_data = [[int(i) for i in x.split(' ')] for x in raw_data[1:]]
-#    #print(all_data)
-#    all_nodes= []
-#    for i in range(0,people):                                       #init all nodes as empty sets O(n)
-#        all_nodes.append(node(id=(i+1))) 
-#    return all_data,people,pairs,all_nodes 
-#
 
 def init():
-    """ Returns the formatted data fetched from stdin. """
     raw_data = []                           
     for input in sys.stdin:
-        raw_data.append(input.strip())     
+        raw_data.append(input.strip())                                      # append every input line to raw_data
 
-    info = raw_data[0].split(' ')
-    people = int(info[0])               # onödig data?
-    pairs = int(info[1])                # onödig data?
-    
-    edges = [[int(i) for i in line.split(' ')] for line in raw_data[1:]]                 
+    num_of_nodes = int(raw_data[0].split(' ')[1])                           # extract number of nodes from first line
+    edges = [[int(i) for i in line.split(' ')] for line in raw_data[1:]]    # extract [u, v, weight] arrays to edge array
 
-
-    nodes= []
-    for i in range(0,people):                                                           #init all nodes as empty sets O(n)
-        nodes.append(node(id=(i+1))) 
-    return edges
+    return edges, num_of_nodes
 
 
-def krus(nodes,data):
-    big_set = []
-    edges = data.copy()
-    print(f'Edges going in {edges}')
-    while len(edges) !=0:
-        current_edge = edges.pop(0)                                                    #taking out the first edge
-        start_node = nodes[current_edge[0]-1]                                          #for reference we take out the nodes as well
-        end_node = nodes[current_edge[1]-1]
-        #print('start_node id :',{start_node.id}, 'and set ',start_node.set)
-        #print(f'end_node id:{end_node.id} and set {end_node.set}')
-        if not union(start_node,end_node):                                             #if they're not in the same set
-            #print(f'Adding edge : {current_edge}')   
-            if start_node.setsize < end_node.setsize:                                  #BUG: need to update the sets so the other elements in that set also connects, NEED A POINTER HERE so it keeps track of the slot instead of the value.       
-                start_node.merge(end_node)
-            else:
-                end_node.merge(start_node)
-            big_set.append(current_edge)                                                #adding the edge to our final set
-        #else do nothing since we don't want this node due to circut
-    return big_set
+def kruskal(edges, n):
+    edges.sort(key=lambda x: x[2])                                          # sort edges in order of weight
+    edges = deque(edges)                                                    # place edges in work queue
+    T = Graph(n)                                                            # initiate graph 
+    mst_edges = []                                                          # list of edges part of minimal spanning tree 
 
-
-def kruskal(edges):
-    edges.sort(key=lambda x: x[2])
-    edges = deque(edges)
-    T = Graph()    
-    mst_edges = []
-
-    while edges:
+    while edges:                                                            # iterate through queue of sorted edges
         current_edge = edges.popleft()
-        if T.find(current_edge[0]) != T.find(current_edge[1]):
-            T.union(current_edge)
-            mst_edges.append(current_edge)
+        if T.find(current_edge[0]) != T.find(current_edge[1]):              # if edges not part of same set...
+            T.union(current_edge)                                           # ...add edge to T-graph
+            mst_edges.append(current_edge)                                  # ...add edge to MST list
     return mst_edges
 
 
-
-def get_cost(edges):
+def print_cost(edges):
     total_cost= 0
-    for e in edges:                                                                 #Getting the total cost for the optimal graph
-        total_cost += e[2]
-    return total_cost
-
-
-
-    
+    for e in edges:                                                         # iterate through MST list...
+        total_cost += e[2]                                                  # calc sum of weights
+    print(total_cost)
         
+
 def main():              
-    data = init()
-    mst_edges = kruskal(data)
-    final_cost = get_cost(mst_edges)
-    print(final_cost)
-    """
-    data,people,pairs,nodes = get_data()
-    final_graph = krus(data=data, nodes=nodes)
-    final_cost = get_cost(final_graph)
-    print(f'Final graph {final_graph}')
-    print(final_cost)
-"""
+    data, n = init()                                                        # O(2*n + 3*n) = O(n), where n = number of lines.
+    mst_edges = kruskal(data, n)                                            # O(n*log(n)[sort] + n[loop] + [find] ) = O(n*log(n))
+    final_cost = get_cost(mst_edges)                                        # O(n-1), where n = number of nodes.
 
 if __name__== "__main__": main()
 
-     
-        
- 
-
-
-
-"""THIS SECTION CONTAINS NOT USED CODE"""
-
-#%%
-"""This is just taken from his psudo code, not used"""
-def makeunionfind(S):
-    x=2
-
-#
-#def kruskal(G):
-#    """Psudo implementation of kruskal"""
-#    T = None
-#    B = E
-#    while len(B)!=0:
-#        #select edge e with minimal weight
-#        #if T U {e} does not create a cycle then:
-#            #add e to T
-#        #remove e from B
-#    return T
-#        
-#       
