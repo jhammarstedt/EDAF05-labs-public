8175b9bd382497d86e3f92af86eedf4e3feb786f
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-05-08 14:47:37 +0200

    inputs

--- 5gorilla/solution.py
+++ 5gorilla/solution.py
@@ -1,21 +1,27 @@
 import sys
-import numpy as np
-import pandas as pd
-
+#import pandas as pd
+#import numpy as np
 def get_data():
-    raw_data = []                           
-    for input in sys.stdin:
-        # append every input line to raw_data
-        raw_data.append(input.strip())                                      
-
-
+    raw_data=[]
     
+    for input in sys.stdin:
+        raw_data.append(input.strip())
+    
+    letters= raw_data.pop(0).split(' ')
+    inputs = [[int(x) for x in i.split(' ')] for i in raw_data[0:len(letters)]]
+    antal = raw_data[len(letters)]
+    strings = raw_data[len(letters)+1].split(' ')
+    
+    costs = pd.DataFrame(data=inpus,columns=letters,index=letters)
     # costs som dataframe, strings som array [ord1, ord2]
-    return costs, strings
+    return costs, strings,antal
 
 
 def main():              
-    costs, strings = get_data()
+    costs, strings,antal = get_data()
+    print(costs)
+    print(strings)
+    print(antal)
     
 
 
