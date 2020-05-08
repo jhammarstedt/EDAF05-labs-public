ce8bd331cce8f9e8ed44fda7ecc040eb72648645
Henrik Norrman <norrman92@gmail.com>
2020-05-08 14:47:46 +0200

    boilerplate

--- 5gorilla/solution.py
+++ 5gorilla/solution.py
@@ -14,9 +14,46 @@ def get_data():
     return costs, strings
 
 
+
+def match_all(costs, strings):
+    results = []
+    for string in strings:
+        results = seq_align(string[0], string[1])
+    return results
+
+
+##################################
+
+
+def seq_align(s, t):
+    ## på något sätt, jämför s och t
+    return [s, t] #returnera modifierade s och t
+
+
+    def opt(i, j, costs):
+        #if i == 0:  j = 0         
+        #if j == 0:  i = 0        
+        delta = -4
+
+        min(costs(x_i,y_i) + opt(i-1, j-1),
+        delta + opt(i, j-1),
+        delta + opt(i-1, j))
+        return min() #kostnad för alla alternativ
+
+
+
+###################################
+
+
+def print_results(results):
+    for result in results:
+        print(result)
+
+
 def main():              
     costs, strings = get_data()
-    
+    results = match_all(costs, strings)
+    print_results(results)
 
 
 if __name__== "__main__": main()
