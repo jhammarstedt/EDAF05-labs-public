60a54a18b93d790d530b263fb58adec72c7c667a
Henrik Norrman <norrman92@gmail.com>
2020-05-10 21:42:03 +0200

    messy again :(

--- 5gorilla/solution.py
+++ 5gorilla/solution.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
+sys.setrecursionlimit(3500)
 
 
 def get_data():
@@ -33,7 +34,47 @@ def traceback(s, t, matrix):
     print("col:",s)
     print("row:",t)
     print(matrix)
-#    row, col = matrix.shape
+
+    row, col = matrix.shape
+    print(col) #=s
+    print(row) #=t
+
+    word_s = ""
+    word_t = ""
+
+    row -= 1
+    col -= 1
+    index = 0
+    while(row and col):
+        diag = matrix[row-1][col-1]
+        col_plus = matrix[row][col-1]
+        row_plus = matrix[row-1][col]
+        
+        comp = max(diag, col_plus, row_plus)
+
+        if diag == comp:
+            word_s += s[len(s)-col-1]
+            word_t += t[len(t)-row-1]
+            row -= 1
+            col -= 1
+        elif col_plus == comp:
+            word_s += s[len(s)-col-1]
+            word_t += '*'
+            #row -= 1
+            col -= 1
+        else:
+            word_s += '*'
+            word_t += t[len(t)-row-1]
+            row -= 1
+            #col -= 1
+        index += 1
+
+    if row == 0 and col == 0:
+        word_s += s[len(s)-col-1]
+        word_t += t[len(t)-row-1]
+
+    
+
 #    word1 = ""
 #    word2 = ""
 #    row -=1
@@ -73,36 +114,61 @@ def traceback(s, t, matrix):
 #
 #
 #    print("word1:",word1 + " " + "word2:",word2)
-
-    return s + " " + t
+    print("s:", word_s, "t:", word_t)
+    return word_s + " " + word_t
 
 def seq_align(s, t, costs):
-    cache = np.zeros((len(s), len(t)))
+    cache = np.zeros((len(s)+1, len(t)+1), dtype="int")
 
-    def opt(index_s, index_t):
+#    def opt(index_s, index_t):
+#        delta = -4
+#
+#        if index_s < 0:
+#            return (index_t+1) * delta
+#        if index_t < 0:
+#            return (index_s+1) * delta
+#
+#        if cache[index_s][index_t] != 0:
+#            return cache[index_s][index_t]
+#
+#        letter_s = s[index_s]
+#        letter_t = t[index_t]
+#    
+#        cache[index_s, index_t] = max(
+#            opt(index_s-1, index_t-1) + costs.loc[letter_s, letter_t], 
+#            opt(index_s, index_t-1) + delta,
+#            opt(index_s-1, index_t) + delta
+#        )
+#        return cache[index_s, index_t]
+#
+
+#    def opt(index_s, index_t):
+#        return 
+
+    def make_table():
+        col = 0
         delta = -4
-        if cache[index_s][index_t] != 0:
-            return cache[index_s][index_t]
+        while col < cache.shape[0]:
+            cache[col][0] = col * delta
+            row = 0
+            while row < cache.shape[1]:
+                cache[0][row] = row * delta
+                if col and row:
+                    diag = cache[col-1][row-1] + costs.loc[s[col-1], t[row-1]]
+                    prev_row = cache[col-1][row] + delta
+                    prev_col = cache[col][row-1] + delta
 
-        if index_s < 0:
-            return (index_t+1) * delta
-        if index_t < 0:
-            return (index_s+1) * delta
-       
-        letter_s = s[index_s]
-        letter_t = t[index_t]
-    
-        cache[index_s, index_t] = max(
-            opt(index_s-1, index_t-1) + costs.loc[letter_s, letter_t], 
-            opt(index_s, index_t-1) + delta,
-            opt(index_s-1, index_t) + delta
-        )
-        return cache[index_s, index_t]
+                    cache[col][row] = max(diag, prev_row, prev_col)
+                row += 1
+            col += 1
 
-    opt(len(s)-1, len(t)-1)
-    words = traceback(s, t, cache)
 
-    return words
+    #opt(len(s)-1, len(t)-1)
+    #words = traceback(s, t, cache)
+
+    make_table()
+    print(cache)
+    return "hello world"
 
 
 def print_results(results):
--- 5gorilla/test.sh
+++ 5gorilla/test.sh
@@ -1,4 +1,6 @@
-python3 solution.py < data/sample/1.in
+#python3 solution.py < data/sample/1.in
 #python3 solution.py < data/secret/0mini.in
 #python3 solution.py < data/secret/1small.in
 #python3 solution.py < data/secret/2med.in
+python3 solution.py < data/secret/3large.in
+#python3 solution.py < data/secret/4huge.in
