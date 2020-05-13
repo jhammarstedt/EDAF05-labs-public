6493cd9638babbe1d8c26653a6c434f1e68307dc
Henrik Norrman <norrman92@gmail.com>
2020-05-13 10:02:22 +0200

    bug fix

--- 5gorilla/solution.py
+++ 5gorilla/solution.py
@@ -39,45 +39,90 @@ def match_all(costs, strings):
 ##################################
 
 def traceback(s, t, matrix,costs):
-
-    row = len(matrix) -1
-    #row = matrix.shape[0] -1
-
-    col = len(matrix[0]) -1
-    #col = matrix.shape[1] -1
-    
+    row = len(s)
+    col = len(t) 
     delta = -4
-    word_s = ""
-    word_t = ""
-    #print(matrix)
-    while row >= 0 and col >= 0:
-        if row == 0 and col ==0:
-            break
-        #print("row:",row,"col:",col)
-        letter_s = s[row-1]
-        letter_t = t[col-1]
+    
+    word_s = word_t = " "
 
-        move_diag = matrix[row][col] + costs[letter_s][letter_t]#costs.loc[letter_s, letter_t]
-        move_col = matrix[row][col-1] + delta
-        move_row = matrix[row-1][col] + delta
+    move_diag = 1
+    move_col = move_row = 0
+    while row and col:
+        #print("row:",row,"col:",col)
+        #if row < 1:
+        #    move_row = move
+        #    move_col = not move
+            #return word_s + " " + '*'*(col-1) + word_t
+
+        #if col <= 0:
+            #return '*'*(row-1) + word_s + " " + word_t
+        #    move_row = not move
+        #    move_col = move
+        #print(s[-1])
+#        letter_s = s[row-1]
+ #       letter_t = t[col-1]
+        #print("s",letter_s,"t",letter_t)
 
         move = max(move_diag, move_col, move_row)
 
-        if move_diag == move: 
-            row -= 1
-            col -= 1
-            word_s = letter_s + word_s  # swap
-            word_t = letter_t + word_t
+        if 1 != 1:
+            print("hello")
+
         elif move_col == move:
             #row -= 1
-            col -= 1
-            word_t = letter_t + word_t  
+            #word_t = letter_t + word_t
+            word_t = t[col-1] + word_t
             word_s = '*' + word_s # C
-        else:
+            col -= 1
+
+        elif move_diag == move: 
+            word_s = s[row-1] + word_s  # swap
+            word_t = t[col-1] + word_t
             row -= 1
+            col -= 1
+    #    print(word_s," ",word_t)
+
+        elif move_row == move:
             word_t = '*' + word_t
-            word_s = letter_s + word_s
+            #word_s = letter_s + word_s
+            word_s = s[row-1] + word_s
+            row -= 1
             #col -= 1
+        else:
+            print("hello")
+        
+        if not col or not row:
+            move_diag = -10
+        else:
+            move_diag = matrix[row-1][col-1] + costs[s[row-1]][t[col-1]]#costs.loc[letter_s, letter_t]
+        move_col = matrix[row][col-1] + delta
+        move_row = matrix[row-1][col] + delta
+
+    if row and not col:
+    
+        word_s = s[row] + word_s
+        word_t = '*'+ word_t
+        #print("row")
+    if col and not row:
+#    while row >= 0 and col >= 0:
+        word_t = t[col] + word_t
+        word_s = '*'+ word_s
+#        if row == 0 and col ==0:
+#            break
+#        #print("row:",row,"col:",col)
+#        letter_s = s[row-1]
+#        letter_t = t[col-1]
+#        #print("row:",row,"letter_s:",letter_s)
+#        #print("col:",col,"letter_t:",letter_t)
+#        if row > 1 and col > 1:
+#            move_diag = matrix[row-1][col-1] + costs[letter_s][letter_t]#costs.loc[letter_s, letter_t]
+#        else:
+#            move_diag = -10
+#        move_col = matrix[row][col-1] + delta
+#        move_row = matrix[row-1][col] + delta
+#
+#        
+#
 
     return word_s + " " + word_t
 
--- 5gorilla/test.sh
+++ 5gorilla/test.sh
@@ -1,12 +1,13 @@
-#python3 solution.py < data/sample/1.in
+#pypy3 solution.py < data/sample/1.in
 #diff data/sample/1.out data/sample/1.ans
 #python3 solution.py < data/secret/0mini.in
 #diff data/secret/1small.out data/secret/1small.ans
-python3 solution.py < data/secret/1small.in
+#pypy3 solution.py < data/secret/1small.in
+#cat data/secret/1small.ans
 #diff data/secret/1small.out data/secret/1small.ans
-#python3 solution.py < data/secret/2med.in
+python3 solution.py < data/secret/2med.in
+diff data/secret/2med.out data/secret/2med.ans
+#pypy3 solution.py < data/secret/3large.in
 #diff data/secret/1small.out data/secret/1small.ans
-#python3 solution.py < data/secret/3large.in
-#diff data/secret/1small.out data/secret/1small.ans
-#python3 solution.py < data/secret/4huge.in
+#pypy3 solution.py < data/secret/4huge.in
 #diff data/secret/1small.out data/secret/1small.ans
