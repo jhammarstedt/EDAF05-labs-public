96c7e03b4b6e74902d1f024f788b2bcfb9e9b6dc
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-05-14 10:44:15 +0200

    starting to modify bfs

--- 6railwayplanning/graph.py
+++ 6railwayplanning/graph.py
@@ -1,35 +1,40 @@
 class Node:
-    def __init__ (self,node,height=0):
-        self.id = node                       
-        #self.connections = []
-        self.height = height                             
-        self.edges = {}
-    def get_neighbours(self):           
+    def __init__ (self,node):
+        self.id = node                                 
+        self.token = 0  
+        self.edges= {}                   
+
+    def get_edges(self):           
         """ Returns every neighbour of the node. """
         return self.edges
 
-    def print_connections(self):
+    def print_edges(self):
         """ Prints every neighbour of the node. """
-        neighbours = [[node[0].id,node[1]] for node in self.edges] #print each node with it's capacity
-        print(self.id,' is connected to [x with capacity y]', neighbours)
+        #print(self.edges[1].cnode.id)
+        neigh = [[self.edges[neighbour].cnode.id,self.edges[neighbour].capacity] for neighbour in self.edges.keys()]
+        #neighbours = [åedge.cnode.id,edge.capacity] for edge in self.edges.keys]
+        print(self.id,' is connected to ', neigh)
 
-    def add_connection(self, neighbour,capacity):
-        """ Connects a neighbour to the node. So you can get this node by nodes[0].edges[neighbour] """
-        self.edges[neighbour.id] = Edge(to_node=neighbour,capacity=capacity)
-        #self.edges.append([neighbour,capacity])
-  
+    def add_edge(self,to_node,capacity):
+        """ Connects a neighbour to the node with an edge. """ 
+        self.edges[to_node.id]=Edge(to_node=to_node,capacity=capacity)
+        
+    def is_visited(self, token):
+        """ Checks if token has been used for visiting the node. """
+        return token <= self.token
+
+    def set_visited(self, token):
+        """ Sets a new visit token. """
+        self.token = token
     
 class Edge:
-    def __init__(self,to_node,capacity=0):
-
+    """Edges are connected through nodes and accessed from each nodes by a dict of edges"""
+    def __init__(self,to_node,capacity):
         self.capacity= capacity             
-        self.flow = self.capacity       #should be set to capacity for all nodes connected to s (node 0 in our case), for the others it starts at 0
-        self.delta=0                    #for the residual
-    
-    def update_flow(self,new_flow):
-        """we will change the flow by delta"""
-        self.flow = new_flow
-        
+        self.flow = 0                   #e
+        self.cnode= to_node             #the other node which it is connected to 
+
+
 class Graph:
     def __init__(self):
         self.nodes = {}
@@ -47,13 +52,70 @@ class Graph:
         else:
             return None
 
-    def add_edge(self, node1, node2,capacity):
-        """ Creates an edge between the nodes {node1} and {node1} with given capacity. """
-        self.nodes[node1].add_connection(self.nodes[node2],capacity)
-        self.nodes[node2].add_connection(self.nodes[node1],capacity) #creating it twice for now since it's undirected
-        
+    def add_edge(self, frm, to,capacity):
+        """ Creates an edge between the nodes {frm} and {to}. """
+        self.nodes[frm].add_edge(to_node =self.nodes[to],capacity=capacity)
+
+    def get_edge(self,frm,to):
+        """Get edge from node to another"""
+        #ed = self.nodes[frm].edges[to]
+        #print(f'Getting edge from {self.nodes[frm].id} to {self.nodes[to].id} with capacity {ed.capacity}')
+        return self.nodes[frm].edges[to]
     def print_graph(self):
         """ Prints the graph. """
         for node in self.nodes:
-            self.nodes[node].print_connections()        
+            self.nodes[node].print_edges()        
     
+
+
+def extra():
+    class Node:
+        def __init__ (self,node,height=0):
+            self.id = node                       
+            #self.connections = []
+            self.height = height                             
+            self.edges = {}
+        def get_neighbours(self):           
+            """ Returns every neighbour of the node. """
+            return self.edges
+
+        def print_connections(self):
+            """ Prints every neighbour of the node. """
+            neighbours = [[node[0].id,node[1]] for node in self.edges] #print each node with it's capacity
+            print(self.id,' is connected to [x with capacity y]', neighbours)
+
+        def add_connection(self, neighbour,capacity):
+            """ Connects a neighbour to the node. So you can get this node by nodes[0].edges[neighbour] """
+            self.edges[neighbour.id] = Edge(to_node=neighbour,capacity=capacity)
+            #self.edges.append([neighbour,capacity])
+    
+        
+
+
+    class Graph:
+        def __init__(self):
+            self.nodes = {}
+            self.num_nodes = 0
+
+        def add_node(self, name):
+            """ Adds a new vertex to the graph. """
+            self.num_nodes += 1
+            self.nodes[name] = Node(name)
+
+        def get_node(self, name):
+            """ Returns the vertex of id {name}, or None if {name} can't be found. """
+            if name in self.nodes:
+                return self.nodes[name]
+            else:
+                return None
+
+        def add_edge(self, node1, node2,capacity):
+            """ Creates an edge between the nodes {node1} and {node1} with given capacity. """
+            self.nodes[node1].add_connection(self.nodes[node2],capacity)
+            self.nodes[node2].add_connection(self.nodes[node1],capacity) #creating it twice for now since it's undirected
+            
+        def print_graph(self):
+            """ Prints the graph. """
+            for node in self.nodes:
+                self.nodes[node].print_connections()        
+        
--- 6railwayplanning/solution.py
+++ 6railwayplanning/solution.py
@@ -17,26 +17,42 @@ def get_data():
 def create_graph(attrib,nodes):
     """This function will take in the attributes from the input and the structure of the graph to create a graph object from the graph.py file"""
     g = Graph()
-    N = attrib[0]                   # number of stations, 
-    for i in range(0,attrib[0]):    #number of nodes we wish to create
-        if i ==0: 
-            g.add_node(i,height=N)  #setting H(s)=N
-        else:g.add_node(i)          # rest of nodes start at 0
+    N = attrib[0]                                  #number of stations, 
+    for i in range(0,attrib[0]):                   #number of nodes we wish to create
+        g.add_node(name=i)
     for i in nodes:
-        g.add_edge(i[0],i[1],i[2])
-
+        g.add_edge(frm=i[0],to=i[1],capacity=i[2]) #first element contains from,second to, third capacity
+        g.add_edge(frm=i[1],to=i[0],capacity=i[2]) 
     g.print_graph()
+    e =g.nodes[0].get_edges()
+    print(e[1].cnode.is_visited())
 
-def push_ready(high_node,low_node):
-    """This is on the residual graph"""
-    residual_graf =' '
-    if (high_node.exessive_flow > 0) and (high_node.height > low_node.height) and ((high_node and low_node) in residual_graf): #implementera
-        return True 
-    if check_forward_edge()
-    
+def BFS(start, goal, graph, token):
+    start_node = graph.get_node(start)
+    goal_node = graph.get_node(goal)
 
-def push(preflow,height,node_v,node_w):
-    if not push_ready(high_node=node_v,low_node=node_w): return 'Not ready to push'#Check 
+    if start == goal:                                   # early out, if same node
+        return 0
+    elif start_node == goal_node:                       # early out, if same tail+head combination
+        return 1
+
+    queue = collections.deque()
+    queue.append(start_node)                            # queue <- first node
+    level = { start_node:0 }                            # level(first node) <- 0
+
+    #a bit unsure of this one
+    while queue:
+        v = queue.popleft()
+        neighbours = v.get_edges()                 # get all neighbours
+        for edge in neighbours:
+            if not edge.cnode.is_visited(token):      # if the connected node not yet visited
+                edge.cnode.set_visited(token)               
+                queue.append(edge)                         # add node n to queue 
+                level[edge.cnode.id] = level[v.id] +1                  # increment level value
+                if edge.cnode == goal_node:
+                    return level[edge.cnode.id]                     # goal found -> return level value
+    return "Impossible"                                 # goal not found -> return "Impossible"
+
 
 def main():
     info,nodes,removal=get_data()
@@ -46,3 +62,21 @@ def main():
     graph = create_graph(info,nodes)
 
 if __name__=='__main__': main()
+
+
+
+def old_main_labb2():                                     
+    elements, queries = get_data()                      # get data from standard input
+    elements_set = create_set(elements)                 # create set of relevant elements
+    graph = create_graph(elements_set)                  # create a graph of the elements from set
+
+    results = []
+    token = 1                                           # initiate visit token to 1
+    for query in queries:
+        start = query[0]                                # start node from query list
+        goal = query[1]                                 # goal noded from query list
+        results.append(BFS(start, goal, graph, token))  # BFS for every query in queries
+        token += 1                                      # increment token
+
+    print_results(results)   
+
