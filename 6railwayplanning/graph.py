fee6c908f1b9b6a604fbe0a160320ea8e8bc0c92
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2020-05-20 10:25:31 +0200

    added limit

---
+++ 6railwayplanning/graph.py
@@ -0,0 +1,134 @@
+class Graph:
+    def __init__(self):
+        self.nodes = {}
+        self.all_edges = {}
+        self.source_node = None
+        self.sink_node = None
+
+    def add_node(self, name):
+        """ Adds a new node to the graph. """
+        self.nodes[name] = Node(name)
+
+    def get_node(self, name):
+        """ Returns the node of id {name}, or None if {name} can't be found. """
+        if name in self.nodes:
+            return self.nodes[name]
+        else:
+            return None
+
+    def print_weights(self):
+        for node in self.nodes:
+            for edge in self.nodes[node].edges:
+                flow = edge.flow
+                cap = edge.capacity
+                print("node:",node,"to",edge.cnode.id,"edge_id",edge.id,"flow:",flow,"cap:",cap)
+
+
+    def add_edge(self, edge_id, frm, to, capacity):
+        """ Creates an edge between the nodes {frm} and {to}. """
+        new_edge = self.nodes[frm].add_edge(edge_id=edge_id, to_node=self.nodes[to], capacity=capacity)
+        self.all_edges[edge_id] = new_edge
+        return new_edge
+
+    def get_edge_by_node(self,frm,to):
+        """Get edge from node to another"""
+        #print("from:",frm,"nodes:",self.nodes.edge_map)
+        return self.nodes[frm].edge_map[to]
+
+    def get_edge_by_id(self, edge_id):
+        return self.all_edges[edge_id]
+
+    def print_graph(self):
+        """ Prints the graph. """
+        for node in self.nodes:
+            self.nodes[node].print_edges()        
+
+    def select_source(self, node_id):
+        self.source_node = self.nodes[node_id]
+
+    def get_source(self):
+        return self.source_node
+
+    def select_sink(self, node_id):
+        self.sink_node = self.nodes[node_id]
+
+    def get_sink(self):
+        return self.sink_node
+
+    def disabled_edge(self, edge_id):
+        self.all_edges[edge_id].disable()
+
+    def reset_edges(self):
+        for edge_id in self.all_edges:
+            self.all_edges[edge_id].set_flow(0)
+    
+    def get_edge_count(self):
+        return len(self.all_edges)
+
+class Node:
+    def __init__ (self,node):
+        self.id = node
+        self.edges = [] 
+        self.edge_map = {}
+
+    def get_edges(self):           
+        """ Returns every neighbour of the node. """
+        return self.edges
+
+    def print_edges(self):
+        """ Prints every neighbour of the node. """
+        neigh = [edge.cnode.id for edge in self.edges]
+        #neigh = [[self.edges[neighbour].cnode.id, self.edges[neighbour].capacity] for neighbour in self.edges.keys()]
+        print(self.id, 'is connected to', neigh)
+
+    def add_edge(self, edge_id, to_node, capacity):
+        """ Connects a neighbour to the node with an edge. """ 
+        new_edge = Edge(edge_id=edge_id, to_node=to_node,capacity=capacity)
+        self.edge_map[to_node.id] = new_edge
+        self.edges.append(new_edge)
+        return new_edge
+
+    #def is_visited(self, token):
+    #    """ Checks if token has been used for visiting the node. """
+    #    return token <= self.token
+
+    #def set_visited(self, token):
+    #    """ Sets a new visit token. """
+    #    self.token = token
+    
+
+class Edge:
+    """Edges are connected through nodes and accessed from each nodes by a dict of edges"""
+    def __init__(self, edge_id, to_node, capacity):
+        self.id = edge_id
+        self.capacity = capacity             
+        self.flow = 0                   #e
+        self.cnode= to_node             #the other node which it is connected to 
+   
+    def disable(self):
+        self.capacity = 0
+
+    def get_capacity(self):
+        return self.capacity
+
+    def set_flow(self, new_flow):
+        self.flow = new_flow
+
+    def add_flow(self, new_flow):
+        self.flow += new_flow
+
+    def get_flow(self):
+        return self.flow
+    
+    def is_available(self,bn_cap=None):
+        if bn_cap==None:
+            return self.capacity - self.flow
+        elif bn_cap <= self.capacity - self.flow:
+            return True
+        else:
+            return False
+
+#class Residual_graph(Graph):
+#      def __init__(self):
+#          super().__init__() #gets all the attributes from G
+
---
+++ 6railwayplanning/solution.py
@@ -0,0 +1,158 @@
+import sys
+import time
+from graph import Graph
+from collections import deque
+import math
+
+def get_data():
+    raw_data=[]
+    
+    for input in sys.stdin:
+        raw_data.append(input.strip())
+
+    metadata = [int(i) for i in raw_data.pop(0).split(' ')]
+    nr_nodes = metadata[0]
+    nr_edges = metadata[1]
+    nr_routes = metadata[3]
+    edges = [[int(i) for i in k.split(' ')] for k in raw_data[:nr_edges]]
+    remove_list = [int(i) for i in raw_data[nr_edges:]]
+    
+    return metadata, edges, remove_list
+
+
+
+def create_graph(nodes, edge_list):
+    graph = Graph()
+
+    # creates every node
+    for i in range(nodes):                  
+        graph.add_node(name=i)
+
+    graph.select_source(0)
+    graph.select_sink(nodes-1)
+
+    # connects every edge
+    for index, edge in enumerate(edge_list):
+        i = index+1
+        graph.add_edge(edge_id=i, frm=edge[0], to=edge[1], capacity=edge[2]) 
+        graph.add_edge(edge_id=-i, frm=edge[1], to=edge[0], capacity=edge[2]) 
+
+    return graph
+
+
+
+def BFS(graph,k):
+    source_node = graph.get_source()
+    limit = int(2**k)
+    for bottleneck in range(limit,0,-1):
+        queue = deque()
+        queue.append(source_node)          
+        parent = {}   
+        visited = {}
+        
+        while queue:
+            v = queue.popleft()
+            neighbours = v.get_edges()       
+            for edge in neighbours:
+                if edge.cnode.id not in visited and edge.is_available(bn_cap=bottleneck):
+                    visited[edge.cnode.id] = True
+                    queue.append(edge.cnode)  
+                    parent[edge.cnode.id] = { "node": v.id, "edge": edge.id }  
+                    if edge.cnode == graph.get_sink():
+                        return parent
+    return [] 
+
+
+
+def ff_parse_bfs(graph, path):
+    sink_id = graph.get_sink().id
+    source_id = graph.get_source().id
+    node_id = sink_id
+    nodes = []
+    edges = []
+    while node_id != source_id:
+        edge_id = path[node_id]["edge"]
+        nodes.append(node_id)
+        edges.append(edge_id)
+        node_id = path[node_id]["node"] 
+    nodes.append(0)
+    return nodes, edges
+
+
+def ff_find_min_flow(graph, edges):
+    min_delta = sys.maxsize
+    for edge_id in edges:
+        edge = graph.get_edge_by_id(edge_id)
+        min_delta = min(min_delta, edge.is_available())
+    return min_delta
+
+
+def ff_set_flow(graph, min_delta, edges, nodes):
+    # forward edge
+    for edge_id in edges:
+        graph.get_edge_by_id(edge_id).add_flow(min_delta)
+
+    # back edge
+    node_index = graph.get_edge_count()
+    for index in range(len(nodes)-1):
+        u = nodes[index] 
+        v = nodes[index + 1]
+        edge = graph.get_edge_by_node(u,v)
+        edge.add_flow(-min_delta)
+
+
+def ford_fulkerson(graph,k):
+    path = BFS(graph,k)
+    min_flow = 0
+    delta = 0
+    while path:
+        nodes, edges = ff_parse_bfs(graph, path)
+
+        min_flow = ff_find_min_flow(graph, edges)
+        # optimering med min_flow,  2^k något.
+        ff_set_flow(graph, min_flow, edges, nodes)
+        delta += min_flow
+        
+        timer = time.time()
+        path = BFS(graph,k)
+    graph.reset_edges()
+    return delta 
+
+def get_k(graph):
+    source_node = graph.get_source()
+    C= 0
+    for edge in source_node.edges:
+        C+= edge.capacity
+    
+    k_power = int(math.log(C)/math.log(2))
+    return k_power
+def remove_edges(graph, remove_list, threshold):
+    edges_removed = -1
+    max_flow = 0
+    remove_edges = True
+    k = get_k(graph)
+    while remove_edges:
+        flow = ford_fulkerson(graph,k)
+        if flow >= threshold:
+            edges_removed += 1
+            next_edge = remove_list[edges_removed]
+            graph.disabled_edge(next_edge+1)
+            graph.disabled_edge(-(next_edge+1))
+            max_flow = flow
+        else:
+            remove_edges = False
+    return str(edges_removed) + " " + str(max_flow)
+
+
+
+def main():
+    metadata, edge_list, remove_list = get_data()
+    nodes = metadata[0]
+    min_students = metadata[2]
+
+    graph = create_graph(nodes, edge_list)
+    result = remove_edges(graph, remove_list, min_students)
+    print(result)
+
+if __name__=='__main__': main()
+
